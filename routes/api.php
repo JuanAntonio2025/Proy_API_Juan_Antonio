3c8352ba37419c7ecf2c49150652a9b63a712f28
JuanAntonio2025 <jasanchezdec@gmail.com>
2026-01-19 12:32:10 +0100

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth',
+], function (){
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+    Route::get('userProfile', [AuthController::class, 'userProfile'])->middleware('auth:api');
+});
+
